33ff961968521eb3eb5b1f71e75081235a373fff
DenisKh <79641502+Gaelleaf@users.noreply.github.com>
2024-03-28 01:08:03 +0500

    brute

--- task_01_substrings/bruteforce.py
+++ task_01_substrings/bruteforce.py
@@ -1,2 +1,17 @@
-def find_all_substrs(source: str, substr: str) -> list[str]:
-    ...
+def find_all_substrs(source: str, substr: str)-> list[str]:
+    n = len(source)
+    m = len(substr)
+    result = []
+    for i in range(n - m + 1):
+        j = 0
+        while j < m and substr[j] == source[i + j]:
+            j += 1
+        if j == m:
+            result.append(i)
+    return result
+
+
+def main():
+    s = input("Enter string:\n")
+    t = input("Enter substring:\n")
+    print(find_all_substrs(s, t))
